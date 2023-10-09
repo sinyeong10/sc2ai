3a0767734fe53d4a1ef9df9429b752c50e99f8aa
V. hs Sin <cbrnt1210@naver.com>
2023-10-09 23:38:23 +0900

    미네랄 -> 가스 일꾼 보냄

--- distribute_workers수정.py
+++ distribute_workers수정.py
@@ -9,25 +9,31 @@ import sys
 
 from sc2.ids.ability_id import AbilityId
 
+import asyncio
+
 #최적화 츙족시킬 일꾼은 self.workers.idle은 잘 분배됨
 #
 
 class IncrediBot(BotAI):
     async def on_step(self, iteration: int):
+        await asyncio.sleep(0.01)
         if iteration == 0:
             self.check = True
-            self.tmp = []
-            self.a = [-1]
+            self.Flag_count = 0
         # print(f"This is my bot in iteration {iteration}, workers: {self.workers}, idle workers: {self.workers.idle}, supply: {self.supply_used}/{self.supply_cap}")
         # print(f"{iteration}, n_workers: {self.workers.amount}, n_idle_workers: {self.workers.idle.amount},", \
         #     f"minerals: {self.minerals}, gas: {self.vespene}, cannons: {self.structures(UnitTypeId.PHOTONCANNON).amount},", \
         #     f"pylons: {self.structures(UnitTypeId.PYLON).amount}, nexus: {self.structures(UnitTypeId.NEXUS).amount}", \
         #     f"gateways: {self.structures(UnitTypeId.GATEWAY).amount}, cybernetics cores: {self.structures(UnitTypeId.CYBERNETICSCORE).amount}", \
         #     f"stargates: {self.structures(UnitTypeId.STARGATE).amount}, voidrays: {self.units(UnitTypeId.VOIDRAY).amount}, supply: {self.supply_used}/{self.supply_cap}")
-
+        
+        #self.custom_distribute_workers 매번 실행해야 초과되는 상황에 좋음
         if self.workers.idle:
-            await self.chat_send(f"이 일꾼때문에 명령함 {self.workers.idle}")
-            await self.custom_distribute_workers()
+            await self.chat_send(f"{iteration} 이 일꾼때문에 명령함 {self.workers.idle}")
+            await self.custom_distribute_workers() #가스 우선
+        elif self.units(UnitTypeId.PROBE).amount > 19 and (iteration%30 == 0):
+            await self.chat_send(f"{iteration} 다 뽑고 체크")
+            await self.custom_distribute_workers() #가스 우선
 
         # begin logic:
         if self.townhalls:  # do we have a nexus?
@@ -35,7 +41,7 @@ class IncrediBot(BotAI):
 
             #4이상 여유로울 때 프로브 생산
             supply_remaining = self.supply_cap - self. supply_used #공급한도-공급량
-            if nexus.is_idle and self.can_afford(UnitTypeId.PROBE) and supply_remaining > 2 and self.units(UnitTypeId.PROBE).amount+2 < 3*(self.structures(UnitTypeId.ASSIMILATOR).amount)+2*len(self.mineral_field.closer_than(10, self.townhalls.first)):
+            if nexus.is_idle and self.can_afford(UnitTypeId.PROBE) and self.units(UnitTypeId.PROBE).amount+2 < 3*(self.structures(UnitTypeId.ASSIMILATOR).amount)+2*len(self.mineral_field.closer_than(10, self.townhalls.first)):
                 await self.chat_send(f"{self.can_afford(UnitTypeId.PROBE)}")
                 await self.chat_send(f"{supply_remaining}, {self.units(UnitTypeId.PROBE).amount}, {3*(self.structures(UnitTypeId.ASSIMILATOR).amount)+2*len(self.mineral_field.closer_than(10, self.townhalls.first))}")
                 # await self.chat_send(self.time_formatted)
@@ -58,6 +64,14 @@ class IncrediBot(BotAI):
                     for vespene in vespenes:
                         if self.can_afford(UnitTypeId.ASSIMILATOR) and not self.already_pending(UnitTypeId.ASSIMILATOR):
                             await self.build(UnitTypeId.ASSIMILATOR, vespene)
+
+            # #인위적 가스 할당
+            elif len(self.gas_buildings.ready) == 1 and self.Flag_count == 0:
+                await self.move_gas(2)
+                self.Flag_count = 1
+            elif len(self.gas_buildings.ready) == 2 and self.Flag_count == 1:
+                await self.move_gas(2)
+                self.Flag_count = 2
 
             elif not self.structures(UnitTypeId.FORGE):  # if we don't have a forge:
                 if self.can_afford(UnitTypeId.FORGE):  # and we can afford one:
@@ -93,6 +107,46 @@ class IncrediBot(BotAI):
                 if AbilityId.EFFECT_CHRONOBOOSTENERGYCOST in abilities:
                     self.do(nexus(AbilityId.EFFECT_CHRONOBOOSTENERGYCOST, target_structure))
 
+    async def move_gas(self, move_worker: int = 1):
+        bases = self.townhalls.ready
+        gas_buildings = self.gas_buildings.ready
+        target_location = []
+
+        #가스일꾼 체크
+        for mining_place in gas_buildings:
+            if mining_place.has_vespene:
+                # get all workers that target the gas extraction site
+                # or are on their way back from it
+                local_workers = self.workers.filter(
+                    lambda unit: unit.order_target == mining_place.tag or
+                    (unit.is_carrying_vespene and unit.order_target == bases.closest_to(mining_place).tag)
+                )
+            if len(local_workers) < 3:
+                print(len(local_workers))
+                target_location.append(mining_place)
+
+        #옮길 미네랄 일꾼 체크
+        for mining_place in bases:
+            # get tags of minerals around expansion
+            local_minerals_tags = {
+                mineral.tag
+                for mineral in self.mineral_field if mineral.distance_to(mining_place) <= 8
+            }
+            # get all target tags a worker can have
+            # tags of the minerals he could mine at that base
+            # get workers that work at that gather site
+            local_workers = self.workers.filter(
+                lambda unit: unit.order_target in local_minerals_tags or
+                (unit.is_carrying_minerals and unit.order_target == mining_place.tag)
+            )
+
+        for _ in range(move_worker): #이 만큼 미네랄에서 가스로 보냄
+            for target_place in target_location: #가스 기준 가장 가까운 일꾼을 일시킴
+                print(target_place, type(target_place))
+                worker = min(local_workers, key=lambda w: target_place.distance_to(w))
+                worker.gather(target_place)
+                local_workers.remove(worker)
+
     async def custom_distribute_workers(self, resource_ratio: float = 2):
         if not self.mineral_field or not self.workers or not self.townhalls.ready:
             return
@@ -116,7 +170,7 @@ class IncrediBot(BotAI):
                     (unit.is_carrying_vespene and unit.order_target == bases.closest_to(mining_place).tag)
                 )
             else:
-                # get tags of minerals around expansion  <p>이동원으로 삼행시 해보겠습니다.<p>
+                # get tags of minerals around expansion
                 local_minerals_tags = {
                     mineral.tag
                     for mineral in self.mineral_field if mineral.distance_to(mining_place) <= 8
@@ -129,6 +183,7 @@ class IncrediBot(BotAI):
                     (unit.is_carrying_minerals and unit.order_target == mining_place.tag)
                 )
             # too many workers
+            # print("부족한 부분", mining_place, difference)
             if difference > 0:
                 for worker in local_workers[:difference]:
                     worker_pool.append(worker)
@@ -136,7 +191,7 @@ class IncrediBot(BotAI):
             # add mining place to deficit bases for every missing worker
             else:
                 deficit_mining_places += [mining_place for _ in range(-difference)]
-
+        # print("모든 deficit_mining_places :", deficit_mining_places)
         # prepare all minerals near a base if we have too many workers
         # and need to send them to the closest patch
         if len(worker_pool) > len(deficit_mining_places):
@@ -145,28 +200,32 @@ class IncrediBot(BotAI):
                 if any(mineral.distance_to(base) <= 8 for base in self.townhalls.ready)
             ]
         # distribute every worker in the pool
-        for worker in worker_pool:
+        for worker in worker_pool: #초과되어 일시킬 일꾼 리스트
             # as long as have workers and mining places
             if deficit_mining_places:
                 # choose only mineral fields first if current mineral to gas ratio is less than target ratio
-                if self.vespene and self.minerals / self.vespene < resource_ratio:
+                if self.vespene and self.minerals / self.vespene < resource_ratio: #지정한 값보다 비율이 작으면 미네랄 선택
                     possible_mining_places = [place for place in deficit_mining_places if not place.vespene_contents]
                 # else prefer gas
-                else:
+                else: #음수면 비율보다 낮아서 무조건 가스 선택
                     possible_mining_places = [place for place in deficit_mining_places if place.vespene_contents]
                 # if preferred type is not available any more, get all other places
-                if not possible_mining_places:
+                if not possible_mining_places: #가스 선택해야하는 데 없어서 미네랄이라도 선택
                     possible_mining_places = deficit_mining_places
+
+                #각 초과된 일꾼이 가장 가까운 자원으로 감
                 # find closest mining place
-                current_place = min(deficit_mining_places, key=lambda place: place.distance_to(worker))
+                # current_place = min(deficit_mining_places, key=lambda place: place.distance_to(worker))
+                current_place = min(possible_mining_places, key=lambda place: place.distance_to(worker))
+                print("current_place", current_place)
                 # remove it from the list
                 deficit_mining_places.remove(current_place)
                 # if current place is a gas extraction site, go there
-                if current_place.vespene_contents:
+                if current_place.vespene_contents: #가스면 가스캠
                     worker.gather(current_place)
                 # if current place is a gas extraction site,
                 # go to the mineral field that is near and has the most minerals left
-                else:
+                else: #미네랄이면 가장 많은 미네랄을 캠
                     local_minerals = (
                         mineral for mineral in self.mineral_field if mineral.distance_to(current_place) <= 8
                     )
@@ -177,6 +236,8 @@ class IncrediBot(BotAI):
             # more workers to distribute than free mining spots
             # send to closest if worker is doing nothing
             elif worker.is_idle and all_minerals_near_base:
+                #각 일꾼은 all_minerals_near_base인 장소 중 가장 가까운 곳에 감
+                print("all_minerals_near_base", all_minerals_near_base)
                 target_mineral = min(all_minerals_near_base, key=lambda mineral: mineral.distance_to(worker))
                 worker.gather(target_mineral)
             else:
