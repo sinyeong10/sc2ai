fa0966c8df31ea95846556e4cee6e537438f9a7e
V. hs Sin <cbrnt1210@naver.com>
2023-11-03 13:31:44 +0900

    비동기-한번만 진행되게 함
    테스트 수정

--- 비동기 프로그래밍 예제/if문에서 lock적용.py
+++ 비동기 프로그래밍 예제/if문에서 lock적용.py
@@ -1,34 +1,34 @@
-#1. threading.Lock()를 통해서 한번만 실행
-import asyncio
-import threading
+# #1. threading.Lock()를 통해서 한번만 실행
+# import asyncio
+# import threading
 
-async def end_display():
-    print("이동을 완료했습니다.")
+# async def end_display():
+#     print("이동을 완료했습니다.")
 
-class GasStation:
-    def __init__(self):
-        self.gas_lock = threading.Lock()# asyncio.Lock()
+# class GasStation:
+#     def __init__(self):
+#         self.gas_lock = threading.Lock()# asyncio.Lock()
 
-    async def refuel(self, car, callback):
-        # 락을 획득하려고 시도
-        if self.gas_lock.acquire(blocking=False): #blocking=False로 락 취득 실패시 대기하지 않음
-            # 락을 획득한 자동차만 주유 진행
-            print(f"{car}에 탑승했습니다...")
-            await asyncio.sleep(2)
-            print(f"{car}로 이동했습니다.")
-            await callback()
-            # 락 해제
-            self.gas_lock.release()
-        else:
-            print(f"이미 사용자는 탑승하여 {car}에 탑승하지 않습니다.")
+#     async def refuel(self, car, callback):
+#         # 락을 획득하려고 시도
+#         if self.gas_lock.acquire(blocking=False): #blocking=False로 락 취득 실패시 대기하지 않음
+#             # 락을 획득한 자동차만 주유 진행
+#             print(f"{car}에 탑승했습니다...")
+#             await asyncio.sleep(2)
+#             print(f"{car}로 이동했습니다.")
+#             await callback()
+#             # 락 해제
+#             self.gas_lock.release()
+#         else:
+#             print(f"이미 사용자는 탑승하여 {car}에 탑승하지 않습니다.")
 
 
-async def main():
-    station = GasStation()
-    tasks = [station.refuel(f"자동차 {i}", end_display) for i in range(10)]
-    await asyncio.gather(*tasks)
+# async def main():
+#     station = GasStation()
+#     tasks = [station.refuel(f"자동차 {i}", end_display) for i in range(10)]
+#     await asyncio.gather(*tasks)
 
-asyncio.run(main())
+# asyncio.run(main())
 
 #2. asyncio.Lock()를 통해서 한번만 실행
 #await self.gas_lock.acquire():는 락을 취득할 때까지 대기함
@@ -49,23 +49,28 @@ class GasStation:
         # 락을 획득한 자동차만 주유 진행
         print(f"{car}에 탑승했습니다...")
         await asyncio.sleep(2)
-        print(f"{car}로 이동했습니다.")
-        await callback() #일이 끝나면 다시 시작할 수 있게 함
+        print(f"{car}로 이동 중")
+        await callback(car) #일이 끝나면 다시 시작할 수 있게 함
         #락안에서 처리하도록 await로 결과 나오고 다음 코드로 감
         # 락 해제
         self.gas_lock.release()
 
-    async def end_display(self): #callback에서 다시 작동할 수 있게 설정
-        self.Flag = False
-        print("이동을 완료했습니다.")
+    async def end_display(self,car): #callback에서 다시 작동할 수 있게 설정
+        self.Flag = True
+        print(f"{car}로 이동을 완료했습니다.")
 
 async def main():
     station = GasStation()
-    tasks = [station.refuel(f"자동차 {i}", station.end_display) for i in range(10)]
+    tasks = [station.refuel(f"자동차 {i}", station.end_display) for i in range(5)]
+    print("비동기 함수의 병렬 실행")
     await asyncio.gather(*tasks)
 
-asyncio.run(main())
+    tasks = [station.refuel(f"자동차 {i}", station.end_display) for i in range(10)]
+    print("\n비동기 함수를 시간이 지남에 따라 순차적으로 비동기 실행")
+    for order in tasks:
+        await asyncio.sleep(0.6)
+        asyncio.create_task(order)
 
-#나중에 명령 다시 실행할 때도 한번만 실행!
-asyncio.run(main())
+    await asyncio.sleep(5)
+
 asyncio.run(main())
